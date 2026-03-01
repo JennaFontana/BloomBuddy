639f36d7308312d73c5f47458abf0a2333c1cbac
Davey5506 <kpdmmvi@gmail.com>
2026-02-28 21:02:24 -0500

    changed ai model

--- scripts/gemini.js
+++ scripts/gemini.js
@@ -4,7 +4,7 @@ const API_KEY = "AIzaSyAh1LoJTTNBprlly1ULTl5fz7V8nu4Nu-c";
 const genAI = new GoogleGenerativeAI(API_KEY);
 
 export async function get_workout(conditionsStr, goalsStr){
-    const genModel = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+    const genModel = genAI.getGenerativeModel({ model: "gemini_2.5_flash_lite" });
 
     const result = await genModel.generateContent(`Do NOT give medical advice! Do not make any text bold. Create a concise workout plan for a person with the following conditions and goals: (${conditionsStr}) and (${goalsStr}). Return the response as an HTML snippet. Use a <ul> for the list of exercises, with each exercise in its own <li> tag. Place any disclaimers in a separate <p> tag at the end. Only use one disclaimer at most!`);
     const response = await result.response;

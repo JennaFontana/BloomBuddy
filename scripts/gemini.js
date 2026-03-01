914f52ef8d05cc7514cf836c2f56bbda1ececb1c
Davey5506 <kpdmmvi@gmail.com>
2026-02-28 20:42:04 -0500

    changed variable declaration

--- scripts/gemini.js
+++ scripts/gemini.js
@@ -1,10 +1,11 @@
 import { GoogleGenerativeAI } from "https://esm.run/@google/generative-ai";
 
-const API_KEY = "INSERT_API_KEY_HERE";
+API_KEY = "INSERT_API_KEY_HERE";
 const genAI = new GoogleGenerativeAI(API_KEY);
 
 export async function get_workout(conditionsStr, goalsStr){
     const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
     const result = await model.generateContent(`Do NOT give medical advice! Do not make any text bold. Create a concise workout plan for a person with the following conditions and goals: (${conditionsStr}) and (${goalsStr}). Return the response as an HTML snippet. Use a <ul> for the list of exercises, with each exercise in its own <li> tag. Place any disclaimers in a separate <p> tag at the end. Only use one disclaimer at most!`);
     const response = await result.response;

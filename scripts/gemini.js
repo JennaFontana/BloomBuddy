97fe35e4813e7fe61b6882a550bb97201bf4c32e
Davey5506 <kpdmmvi@gmail.com>
2026-03-01 01:43:23 -0500

    Updated api call

--- scripts/gemini.js
+++ scripts/gemini.js
@@ -4,11 +4,12 @@ const ai = new GoogleGenAI({
     apikey: 'INSERT_API_KEY'
 });
 
-async function get_workout(){
-    const response = await ai.generateText({
+async function get_workout(conditionsStr, goalsStr) {\
+    const response = await ai.models.generateContent({
         model: 'gemini-3-flash-preview',
         prompt: "Do NOT give medical advice! Do not make any text bold. Create a concise workout plan for a person with the following conditions and goals: (${conditionsStr}) and (${goalsStr}). Return the response as an HTML snippet. Use a <ul> for the list of exercises, with each exercise in its own <li> tag. Place any disclaimers in a separate <p> tag at the end. Only use one disclaimer at most!"
-    });
-    console.log(response.response);
-
+    })
+    return response.text;
 }
+    
+

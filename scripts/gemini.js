e3221efb9c5a1aa20a731a75e2cce4027ab82287
Davey5506 <kpdmmvi@gmail.com>
2026-03-01 01:40:02 -0500

    Revert "Tuned promt"
    This reverts commit e1181d2e2b3d8ccd7447f8146bc7c2e39da31e20.

--- scripts/gemini.js
+++ scripts/gemini.js
@@ -1,13 +1,14 @@
-import { GoogleGenerativeAI } from "https://esm.run/@google/generative-ai";
+import {GoogleGenAI} from "@google/genai";
 
-const API_KEY = "INSERT_API_KEY";
-const genAI = new GoogleGenerativeAI(API_KEY);
+const ai = new GoogleGenAI({
+    apikey: 'INSERT_API_KEY'
+});
 
-export async function get_workout(conditionsStr, goalsStr){
-    const result = await genAI.model.generateContent(
-        model="gemini-2.5-flash",
-        content=`Do NOT give medical advice! Do not make any text bold. Create a concise workout plan for a person with the following conditions and goals: (${conditionsStr}) and (${goalsStr}). Return the response as an HTML snippet. Use a <ul> for the list of exercises, with each exercise in its own <li> tag. Place any disclaimers in a separate <p> tag at the end. Only use one disclaimer at most!`
-    );
-    const response = await result.response;
-    return response.text();
+async function get_workout(){
+    const response = await ai.generateText({
+        model: 'gemini-3-flash-preview',
+        prompt: "Do NOT give medical advice! Do not make any text bold. Create a concise workout plan for a person with the following conditions and goals: (${conditionsStr}) and (${goalsStr}). Return the response as an HTML snippet. Use a <ul> for the list of exercises, with each exercise in its own <li> tag. Place any disclaimers in a separate <p> tag at the end. Only use one disclaimer at most!"
+    });
+    console.log(response.response);
+
 }
